a2f635c8897130536a0f8a253bd9d89eca7cd9f3
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-10-30 18:12:21 +0000

    SQL script that ranks origins of bands, ordered by number of fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -5,7 +5,7 @@ SOURCE metal_bands.sql
 CREATE INDEX idx_origin ON metal_bands(origin);
 CREATE INDEX idx_nb_fans ON metal_bands(nb_fans);
 
-SELECT origin, SUM(nb_fans) AS total_fans
+SELECT origin, SUM(fans) AS total_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY total_fans;
